eeed360bb4bdc87212d66f14d32d4f46e5eb887b
bowsr <bowserm@outlook.com>
2022-08-20 23:49:34 -0700

    Added firewall rule handling

---
+++ DESpeedrunUtil/Firewall/FirewallHandler.cs
@@ -0,0 +1,33 @@
+﻿using NetFwTypeLib;
+
+namespace DESpeedrunUtil.Firewall {
+    internal class FirewallHandler {
+
+        public static bool CheckForFirewallRule(string application, bool delete) {
+            INetFwPolicy2 policy2 = (INetFwPolicy2) Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+
+            foreach(INetFwRule rule in policy2.Rules) {
+                if(rule.Direction == NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_OUT && rule.ApplicationName == application) {
+                    if(delete) policy2.Rules.Remove(rule.Name);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static void CreateFirewallRule(string application) {
+            INetFwPolicy2 policy2 = (INetFwPolicy2) Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+
+            INetFwRule2 fwRule = (INetFwRule2) Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FWRule"));
+            fwRule.Enabled = true;
+            fwRule.Action = NET_FW_ACTION_.NET_FW_ACTION_BLOCK;
+            fwRule.Direction = NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_OUT;
+            fwRule.Name = "DOOMEternal (SR Utility)";
+            fwRule.ApplicationName = application;
+            fwRule.Profiles = policy2.CurrentProfileTypes;
+
+            policy2.Rules.Add(fwRule);
+        }
+
+    }
+}
