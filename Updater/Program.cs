f3b583b5162ba6bdceb913f81fcf58aa0d4d0a27
bowsr <bowserm@outlook.com>
2022-09-26 21:22:21 -0700

    Add logging and version arg parsing

--- Updater/Program.cs
+++ Updater/Program.cs
@@ -1,14 +1,53 @@
+using Serilog;
+using System.Diagnostics;
+
 namespace Updater {
     internal static class Program {
+
+        private static readonly Version EMPTY_VERSION = new();
+
+        public static Version NewVersion { get; private set; } = EMPTY_VERSION;
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main() {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+        static void Main(string[] args) {
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File(@".\logs\updater\updater_.log", rollingInterval: RollingInterval.Day)
+                .MinimumLevel.Verbose()
+                .CreateLogger();
+
+            if(args.Length > 0) {
+                for(int i = 0; i < args.Length; i++) {
+                    var s = args[i];
+                    try {
+                        NewVersion = ValidateVersionString(s);
+                        if(NewVersion != EMPTY_VERSION) break;
+                    }catch(Exception e) {
+                        Log.Error(e, "\"{String}\" is not a valid version string.", s);
+                    }
+                }
+            }
+            if(NewVersion != EMPTY_VERSION) {
+                Application.Run(new Form1());
+            }else {
+                Log.Error("No valid version was passed. Aborting.");
+            }
+            Log.CloseAndFlush();
+        }
+
+        private static Version ValidateVersionString(string ver) {
+            try {
+                var v = new Version(ver);
+                if(v.Major == 1 && v.Minor >= 0 && v.Build >= 0 && v.Revision == -1)
+                    return v;
+            }catch(Exception) {
+                throw;
+            }
+            return EMPTY_VERSION;
         }
     }
 }
