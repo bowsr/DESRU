f585f260d8532f0d6d71da78df763cba67c07c76
bowsr <bowserm@outlook.com>
2022-10-07 04:10:12 -0700

    Disabled maximize button on the UpdateDialog

--- DESpeedrunUtil/UpdateDialog.Designer.cs
+++ DESpeedrunUtil/UpdateDialog.Designer.cs
@@ -144,6 +144,7 @@
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "UpdateDialog";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "Update Required";
