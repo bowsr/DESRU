4351b926eb28c35c5d8b815305c2ba8697b44218
bowsr <bowserm@outlook.com>
2022-12-30 11:29:26 -0800

    Updated meath00k to 7.2

--- DESpeedrunUtil/Program.cs
+++ DESpeedrunUtil/Program.cs
@@ -8,7 +8,7 @@ using System.Reflection;
 namespace DESpeedrunUtil {
     internal static class Program {
 
-        public const string APP_VERSION = "1.2.6";
+        public const string APP_VERSION = "1.2.7";
         public static bool UpdateDetected = false;
         private static string _latestVersion, _changelog;
         private static bool _checkFailed = false;
