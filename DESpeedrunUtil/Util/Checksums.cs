2031c61765e1ef909a99c130ae85765d437dabf8
bowsr <bowserm@outlook.com>
2023-05-28 16:21:11 -0700

    Added some utility classes for future features

---
+++ DESpeedrunUtil/Util/Checksums.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+
+namespace DESpeedrunUtil.Util {
+    internal static class Checksums {
+
+        internal static string GetMD5ChecksumFromFile(string filePath) {
+            using var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            using var md5 = MD5.Create();
+            return BitConverter.ToString(md5.ComputeHash(fs)).Replace("-", "").ToLower();
+        }
+
+        internal static bool Compare(string checksum0, string checksum1) => checksum0.Replace("-", "").ToLower().Equals(checksum1.Replace("-", "").ToLower());
+        internal static bool CompareFromFile(string filePath, string checksum) => GetMD5ChecksumFromFile(filePath).Equals(checksum.Replace("-", "").ToLower());
+
+    }
+}
---
+++ DESpeedrunUtil/Util/StringUtil.cs
@@ -0,0 +1,31 @@
+﻿namespace DESpeedrunUtil.Util {
+    internal static class StringUtil {
+
+        /// <summary>
+        /// Separates a single hex representation of an array of bytes
+        /// into an array of strings each representing a single byte.
+        /// </summary>
+        /// <param name="hex">Hex String representation of the array of bytes</param>
+        /// <returns>A string array with each value representing a single byte</returns>
+        /// <exception cref="ArgumentException"></exception>
+        internal static string[] SeparateHexIntoArray(string hex) {
+            if(!IsValidHexString(hex)) throw new ArgumentException(hex + " is not a valid hex string.");
+
+            string[] array = new string[hex.Length / 2];
+            for(int i = 0; i < hex.Length / 2; i++)
+                array[i] = hex[(i * 2)..((i + 1) * 2)];
+
+            return array;
+        }
+
+        /// <summary>
+        /// Converts a hex string into an array of bytes.
+        /// </summary>
+        /// <param name="hex">Hex String representation of the array of bytes</param>
+        /// <returns>An array of bytes</returns>
+        internal static byte[] ConvertHexStringToBytes(string hex) => SeparateHexIntoArray(hex).Select(b => Convert.ToByte(b, 16)).ToArray();
+
+        private static bool IsValidHexString(string hex) => hex.Length % 2 == 0 && hex.ToLower().All(c => "0123456789abcdef".Contains(c));
+
+    }
+}
