85deb853ac309c6b38318e68fdc322b23533c668
bowsr <bowserm@outlook.com>
2023-05-31 06:24:00 -0700

    Added an extended settings page for cvars and future options

---
+++ DESpeedrunUtil/SettingsPage.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace DESpeedrunUtil {
+    partial class SettingsPage {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            settingsCVARLabel = new Label();
+            settingsAACheckbox = new CheckBox();
+            settingsUNCheckbox = new CheckBox();
+            settingsAutoContinueCheckbox = new CheckBox();
+            settingsAADescription = new TextBox();
+            settingsUNDescription = new TextBox();
+            settingsAutoContinueDescription = new TextBox();
+            settingsCloseButton = new Button();
+            SuspendLayout();
+            // 
+            // settingsCVARLabel
+            // 
+            settingsCVARLabel.AutoSize = true;
+            settingsCVARLabel.Font = new Font("Eternal UI 2", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            settingsCVARLabel.ForeColor = Color.FromArgb(  230,   230,   230);
+            settingsCVARLabel.Location = new Point(12, 9);
+            settingsCVARLabel.Name = "settingsCVARLabel";
+            settingsCVARLabel.Size = new Size(98, 33);
+            settingsCVARLabel.TabIndex = 0;
+            settingsCVARLabel.Text = "CVARS";
+            // 
+            // settingsAACheckbox
+            // 
+            settingsAACheckbox.AutoSize = true;
+            settingsAACheckbox.Font = new Font("Eternal UI 2", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            settingsAACheckbox.ForeColor = Color.FromArgb(  230,   230,   230);
+            settingsAACheckbox.Location = new Point(27, 45);
+            settingsAACheckbox.Name = "settingsAACheckbox";
+            settingsAACheckbox.Size = new Size(158, 22);
+            settingsAACheckbox.TabIndex = 1;
+            settingsAACheckbox.Tag = "antialiasing";
+            settingsAACheckbox.Text = "Disable Anti-Aliasing";
+            settingsAACheckbox.UseVisualStyleBackColor = true;
+            settingsAACheckbox.CheckedChanged += Checkbox_CheckedChanged;
+            // 
+            // settingsUNCheckbox
+            // 
+            settingsUNCheckbox.AutoSize = true;
+            settingsUNCheckbox.Font = new Font("Eternal UI 2", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            settingsUNCheckbox.ForeColor = Color.FromArgb(  230,   230,   230);
+            settingsUNCheckbox.Location = new Point(27, 98);
+            settingsUNCheckbox.Name = "settingsUNCheckbox";
+            settingsUNCheckbox.Size = new Size(273, 22);
+            settingsUNCheckbox.TabIndex = 2;
+            settingsUNCheckbox.Tag = "undelay";
+            settingsUNCheckbox.Text = "Disable Ultra-Nightmare Quitout Delay";
+            settingsUNCheckbox.UseVisualStyleBackColor = true;
+            settingsUNCheckbox.CheckedChanged += Checkbox_CheckedChanged;
+            // 
+            // settingsAutoContinueCheckbox
+            // 
+            settingsAutoContinueCheckbox.AutoSize = true;
+            settingsAutoContinueCheckbox.Font = new Font("Eternal UI 2", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            settingsAutoContinueCheckbox.ForeColor = Color.FromArgb(  230,   230,   230);
+            settingsAutoContinueCheckbox.Location = new Point(27, 151);
+            settingsAutoContinueCheckbox.Name = "settingsAutoContinueCheckbox";
+            settingsAutoContinueCheckbox.Size = new Size(242, 22);
+            settingsAutoContinueCheckbox.TabIndex = 3;
+            settingsAutoContinueCheckbox.Tag = "autocontinue";
+            settingsAutoContinueCheckbox.Text = "Auto Continue in Loading Screens";
+            settingsAutoContinueCheckbox.UseVisualStyleBackColor = true;
+            settingsAutoContinueCheckbox.CheckedChanged += Checkbox_CheckedChanged;
+            // 
+            // settingsAADescription
+            // 
+            settingsAADescription.BackColor = Color.FromArgb(  35,   35,   35);
+            settingsAADescription.BorderStyle = BorderStyle.None;
+            settingsAADescription.Font = new Font("Eternal UI 2", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            settingsAADescription.ForeColor = Color.FromArgb(  230,   230,   230);
+            settingsAADescription.Location = new Point(48, 65);
+            settingsAADescription.Multiline = true;
+            settingsAADescription.Name = "settingsAADescription";
+            settingsAADescription.ReadOnly = true;
+            settingsAADescription.Size = new Size(315, 32);
+            settingsAADescription.TabIndex = 5;
+            settingsAADescription.Text = "NOTE: This setting does nothing if you have DLSS enabled.";
+            // 
+            // settingsUNDescription
+            // 
+            settingsUNDescription.BackColor = Color.FromArgb(  35,   35,   35);
+            settingsUNDescription.BorderStyle = BorderStyle.None;
+            settingsUNDescription.Font = new Font("Eternal UI 2", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            settingsUNDescription.ForeColor = Color.FromArgb(  230,   230,   230);
+            settingsUNDescription.Location = new Point(48, 118);
+            settingsUNDescription.Multiline = true;
+            settingsUNDescription.Name = "settingsUNDescription";
+            settingsUNDescription.ReadOnly = true;
+            settingsUNDescription.Size = new Size(315, 32);
+            settingsUNDescription.TabIndex = 6;
+            settingsUNDescription.Text = "Removes the delay on some versions before you're able to leave an Ultra-Nightmare run.";
+            // 
+            // settingsAutoContinueDescription
+            // 
+            settingsAutoContinueDescription.BackColor = Color.FromArgb(  35,   35,   35);
+            settingsAutoContinueDescription.BorderStyle = BorderStyle.None;
+            settingsAutoContinueDescription.Font = new Font("Eternal UI 2", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            settingsAutoContinueDescription.ForeColor = Color.FromArgb(  230,   230,   230);
+            settingsAutoContinueDescription.Location = new Point(48, 171);
+            settingsAutoContinueDescription.Multiline = true;
+            settingsAutoContinueDescription.Name = "settingsAutoContinueDescription";
+            settingsAutoContinueDescription.ReadOnly = true;
+            settingsAutoContinueDescription.Size = new Size(315, 32);
+            settingsAutoContinueDescription.TabIndex = 7;
+            settingsAutoContinueDescription.Text = "Forces loading screens to continue into the next level without waiting for user input.";
+            // 
+            // settingsCloseButton
+            // 
+            settingsCloseButton.BackColor = Color.FromArgb(  70,   70,   70);
+            settingsCloseButton.FlatAppearance.BorderColor = SystemColors.WindowFrame;
+            settingsCloseButton.FlatStyle = FlatStyle.Flat;
+            settingsCloseButton.Font = new Font("Eternal UI 2", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            settingsCloseButton.ForeColor = Color.FromArgb(  230,   230,   230);
+            settingsCloseButton.Location = new Point(232, 220);
+            settingsCloseButton.Name = "settingsCloseButton";
+            settingsCloseButton.Size = new Size(150, 28);
+            settingsCloseButton.TabIndex = 8;
+            settingsCloseButton.Text = "close";
+            settingsCloseButton.UseVisualStyleBackColor = false;
+            settingsCloseButton.Click += CloseButton_Click;
+            // 
+            // SettingsPage
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(  35,   35,   35);
+            ClientSize = new Size(394, 260);
+            Controls.Add(settingsCloseButton);
+            Controls.Add(settingsAutoContinueDescription);
+            Controls.Add(settingsUNDescription);
+            Controls.Add(settingsAADescription);
+            Controls.Add(settingsAutoContinueCheckbox);
+            Controls.Add(settingsUNCheckbox);
+            Controls.Add(settingsAACheckbox);
+            Controls.Add(settingsCVARLabel);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "SettingsPage";
+            SizeGripStyle = SizeGripStyle.Hide;
+            Text = "More Settings";
+            TopMost = true;
+            FormClosing += SettingsPage_FormClosing;
+            Load += SettingsPage_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label settingsCVARLabel;
+        private CheckBox settingsAACheckbox;
+        private CheckBox settingsUNCheckbox;
+        private CheckBox settingsAutoContinueCheckbox;
+        private TextBox settingsAADescription;
+        private TextBox settingsUNDescription;
+        private TextBox settingsAutoContinueDescription;
+        private Button settingsCloseButton;
+    }
+}
---
+++ DESpeedrunUtil/SettingsPage.cs
@@ -0,0 +1,65 @@
+﻿using DESpeedrunUtil.Memory;
+
+namespace DESpeedrunUtil {
+    internal partial class SettingsPage: Form {
+
+        MemoryHandler? _memory;
+        MainWindow _parent;
+
+        private bool _enableEvents = false;
+
+        public SettingsPage(MemoryHandler? memory, MainWindow parent) {
+            InitializeComponent();
+            _memory = memory;
+            _parent = parent;
+            settingsAACheckbox.Checked = !Properties.Settings.Default.AntiAliasing;
+            settingsUNCheckbox.Checked = !Properties.Settings.Default.UNDelay;
+            settingsAutoContinueCheckbox.Checked = Properties.Settings.Default.AutoContinue;
+            SetFonts();
+            _enableEvents = true;
+        }
+
+        private void Checkbox_CheckedChanged(object sender, EventArgs e) {
+            if(!_enableEvents) return;
+            var cb = (CheckBox) sender;
+            string tag = cb.Tag.ToString();
+            bool state = tag switch {
+                "antialiasing" or "undelay" => !cb.Checked,
+                "autocontinue" => cb.Checked,
+                _ => false
+            };
+            _memory?.SetCVAR(state, tag);
+            UpdateSettings();
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e) {
+            UpdateSettings();
+            this.Close();
+        }
+
+        private void UpdateSettings() {
+            Properties.Settings.Default.AntiAliasing = !settingsAACheckbox.Checked;
+            Properties.Settings.Default.UNDelay = !settingsUNCheckbox.Checked;
+            Properties.Settings.Default.AutoContinue = settingsAutoContinueCheckbox.Checked;
+        }
+
+        private void SetFonts() {
+            settingsCVARLabel.Font = MainWindow.EternalUIRegular20;
+            settingsAACheckbox.Font = MainWindow.EternalUIRegular;
+            settingsUNCheckbox.Font = MainWindow.EternalUIRegular;
+            settingsAutoContinueCheckbox.Font = MainWindow.EternalUIRegular;
+            settingsCloseButton.Font = MainWindow.EternalUIRegular;
+            settingsAADescription.Font = MainWindow.EternalUIRegular10;
+            settingsUNDescription.Font = MainWindow.EternalUIRegular10;
+            settingsAutoContinueDescription.Font = MainWindow.EternalUIRegular10;
+        }
+
+        private void SettingsPage_Load(object sender, EventArgs e) {
+            this.Location = new Point(_parent.Location.X + 15, _parent.Location.Y + 50);
+        }
+
+        private void SettingsPage_FormClosing(object sender, FormClosingEventArgs e) {
+            _parent?.ToggleButtonStates("settings", true);
+        }
+    }
+}
