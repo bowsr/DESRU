0dbd1598ec93fcf8fe668a4702b29b5f6a59ec06
bowsr <bowserm@outlook.com>
2024-02-17 12:56:16 -0800

    Simplified mod checksum verification code

--- DESpeedrunUtil/Util/Checksums.cs
+++ DESpeedrunUtil/Util/Checksums.cs
@@ -5,17 +5,17 @@ using System.Security.Cryptography;
 namespace DESpeedrunUtil.Util {
     internal static class Checksums {
 
-        private static Dictionary<string, string> _vanillaHashes = new();
-        private static Dictionary<string, string> _srmodHashes = new();
+        private static Dictionary<string, string> _vanillaChecksums = new();
+        private static Dictionary<string, string> _srmodChecksums = new();
 
-        internal static void InitHashDict(string data, string dict) {
+        internal static void InitChecksumDict(string data, string dict) {
             var deserialized = JsonConvert.DeserializeObject<Dictionary<string, string>>(data);
             switch(dict) {
                 case "vanilla":
-                    _vanillaHashes = deserialized;
+                    _vanillaChecksums = deserialized;
                     break;
                 case "srmod":
-                    _srmodHashes = deserialized;
+                    _srmodChecksums = deserialized;
                     break;
                 default:
                     return;
@@ -23,33 +23,29 @@ namespace DESpeedrunUtil.Util {
         }
 
         internal static string VerifyGameDataFiles(string gameDir) {
+            if(gameDir.ToLower().Contains(@"\windowsapps\")) return "UWP";
             string baseDir = gameDir + "\\base\\";
             var isModded = false;
             try {
-                foreach(var key in _vanillaHashes.Keys) {
-                    if(!CompareFromFile(baseDir + key, _vanillaHashes[key])) {
-                        isModded = true;
-                        Log.Information("Game data files have been modified. Checking if file signatures match known mods...");
-                        break;
-                    }
+                Log.Information("Checking if game resource file checksums match any known mods...");
+                // Vanilla
+                if(VerifyChecksums(baseDir, ref _vanillaChecksums)) {
+                    Log.Information("Game resource files have not been modified. Likely that no mods were installed.");
+                    return "vanilla";
                 }
-                if(!isModded) return "vanilla";
-                foreach(var key in _srmodHashes.Keys) {
-                    if(!CompareFromFile(baseDir + key, _srmodHashes[key])) {
-                        Log.Information("Game data files do not match SRMod signatures.");
-                        break;
-                    }
-                    Log.Information("Match detected: SRMod is currently installed.");
+                // SRMod
+                if(VerifyChecksums(baseDir, ref _srmodChecksums)) {
+                    Log.Information("Known mod match found: Speedrun Mod by DrLa");
                     return "srmod";
                 }
 
                 //TODO - proteh's Horde Mode hashes
             } catch(KeyNotFoundException knfe) {
-                Log.Error(knfe, "Failed to verify file hash.");
+                Log.Error(knfe, "Failed to verify checksum.");
                 return "failed";
             }
             Log.Warning("Could not identify installed mods.");
-            Log.Warning("If you have a leaderboard legal mod (e.g. SpeedrunMod by DrLa), it must be the only mod installed, otherwise DESRU cannot verify its installation.");
+            Log.Warning("If you have a leaderboard legal mod installed (e.g. SpeedrunMod by DrLa), it must be the only mod, otherwise DESRU cannot verify its installation.");
             return "unknown";
         }
 
@@ -62,6 +58,13 @@ namespace DESpeedrunUtil.Util {
 
         internal static bool Compare(string checksum0, string checksum1) => checksum0.Replace("-", "").ToLower().Equals(checksum1.Replace("-", "").ToLower());
         internal static bool CompareFromFile(string filePath, string checksum) => GetMD5ChecksumFromFile(filePath).Equals(checksum.Replace("-", "").ToLower());
+
+        private static bool VerifyChecksums(string dir, ref Dictionary<string, string> checksums) {
+            foreach(var key in checksums.Keys)
+                if(!CompareFromFile(dir + key, checksums[key])) 
+                    return false;
+            return true;
+        }
 
     }
 }
